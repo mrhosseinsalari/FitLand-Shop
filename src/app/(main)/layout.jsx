a66db7960eb2f5b1a6638c638c4b33b9c27a1aae
Hossein Salari <hosein.salari81@gmail.com>
2025-04-24 02:27:57 +0330

    add simple top-heading text to layout

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -4,6 +4,11 @@ import React from "react";
 function Layout({ children }) {
   return (
     <>
+      <div className="h-10 bg-secondary-main text-neutral-white flex justify-center items-center">
+        <p className="text-xs font-medium md:text-sm md:font-bold">
+          با عضویت در فیت لند اولین ارسال را مهمون ما باش :)
+        </p>
+      </div>
       <Header />
       <div className="container xl:max-w-screen-xl">{children}</div>
     </>
